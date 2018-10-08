8f6cdf58ebec88391ee8f289abda48d4f5932d63
Gerardo Gudiño García <areg_4@hotmail.com>
2018-10-08 12:46:56 -0500

    Se agregaron los modelos para la BD

--- administradorEVM/models.py
+++ administradorEVM/models.py
@@ -12,3 +12,32 @@ class Users(models.Model):
 	nombre = models.CharField(max_length=250, blank=True,null=True)
 	perfil = models.IntegerField(blank=True, null=True)
 
+class Proyectos(models.Model):
+	"""docstring for Proyectos"""
+	nombreProyecto = models.CharField(max_length=250, blank=True, null=True)
+	resumen = models.TextField(blank=True, null=True)
+	objetivoPrincipal = models.CharField(max_length=250, blank=True, null=True)
+	objetivos = models.TextField(blank=True, null=True)
+	fechaInicio = models.DateField(auto_now=False, blank=True, null=True)
+	fechaFinal = models.DateField(auto_now=False, blank=True, null=True)
+	idResponsable = models.IntegerField(blank=True, null=True)
+
+class Proyectos_Usuarios(models.Model):
+	"""docstring for Proyectos_Usuarios"""
+	idProyecto = models.IntegerField(blank=True, null=True)
+	idUsuario = models.IntegerField(blank=True, null=True)
+
+class Actividades(models.Model):
+	"""docstring for Actividades"""
+	idActProy = models.CharField(max_length=10, blank=True, null=True)
+	idDependencia =  models.IntegerField(blank=True, null=True)
+	fechaEntrega = models.DateField(auto_now=False, blank=True, null=True)
+	descripcion = models.TextField(blank=True, null=True)
+	comentario = models.TextField(blank=True, null=True)
+	hrsPlaneadas = models.FloatField(blank=True, null=True)
+	idResponsable = models.IntegerField(blank=True, null=True)
+	idAutoriza = models.IntegerField(blank=True, null=True)
+	idSoporte = models.IntegerField(blank=True, null=True)
+	idInformar = models.IntegerField(blank=True, null=True)
+	progreso = models.FloatField(blank=True, null=True)
+	tiempoActual = models.FloatField(blank=True, null=True)
